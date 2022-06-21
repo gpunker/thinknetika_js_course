24c0a26cfdbc87e492a5e10eaa831ef8078c1c8c
Alexandr Golovachev <gpunker@gmail.com>
2022-06-21 18:40:29 +0700

    + ternal if task

---
+++ lesson1/ternal_if.js
@@ -0,0 +1,36 @@
+console.log('Проверка прав с помощью тернального оператора if')
+
+function checkAccess(user) {
+    (user.isAdmin && (user.age >= 18 && user.age <= 35)) ||
+      ((user.age >= 18 && user.age <= 35) && user.paid && !user.blocked && !user.badUsername) ? console.log('Доступ разрешен.') : console.log('Доступ запрещен.')
+}
+
+console.log('Простой пользователь:')
+var user = {
+    age: 18,
+    paid: true,
+    blocked: false,
+    badUsername: false,
+    isAdmin: false
+}
+checkAccess(user)
+
+console.log('Пользователь-администратор:')
+var user = {
+    age: 18,
+    paid: true,
+    blocked: false,
+    badUsername: false,
+    isAdmin: true
+}
+checkAccess(user)
+
+console.log('Пользователь не оплатил подписку:')
+var user = {
+    age: 18,
+    paid: true,
+    blocked: true,
+    badUsername: false,
+    isAdmin: false
+}
+checkAccess(user)
