153bc9693a3bbff0d32328a3b8ffd4afc99a82d5
Alexandr Golovachev <gpunker@gmail.com>
2022-06-21 18:35:27 +0700

    + classic if task

---
+++ lesson1/classic_if.js
@@ -0,0 +1,43 @@
+function checkAccess(user) {
+    if (user.isAdmin && (user.age >= 18 && user.age <= 35)) {
+        console.log('Пользователь является администратором. Доступ разрешен.')
+        return true
+    } else if ((user.age >= 18 && user.age <= 35) && user.paid && !user.blocked && !user.badUsername) {
+        console.log('Доступ разрешен.')
+        return true
+    } else {
+        console.log('Доступ запрещен.')
+        return false
+    }
+}
+
+console.log('Проверка прав')
+console.log('Простой пользователь:')
+var user = {
+    age: 18,
+    paid: true,
+    blocked: false,
+    badUsername: false,
+    isAdmin: false
+}
+checkAccess(user)
+
+console.log('Пользователь-администратор:')
+var user = {
+    age: 18,
+    paid: true,
+    blocked: false,
+    badUsername: false,
+    isAdmin: true
+}
+checkAccess(user)
+
+console.log('Пользователь не оплатил подписку:')
+var user = {
+    age: 18,
+    paid: true,
+    blocked: true,
+    badUsername: false,
+    isAdmin: false
+}
+checkAccess(user)
