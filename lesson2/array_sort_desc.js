76bdb52da9e2f0dd56cf260b2d525447e741468e
Alexandr Golovachev <gpunker@gmail.com>
2022-06-24 13:12:08 +0700

    + array desc sorting

---
+++ lesson2/array_sort_desc.js
@@ -0,0 +1,29 @@
+/**
+ * Реализовать функцию для сортировки массива чисел по убыванию
+ * const numbers = [-20, -10, 0, 10, 20, 30]
+ *
+ * sortDesc(numbers) // [30, 20, 10, 0, -10, -20]
+ */
+
+function sortDesc1(array) {
+    return array.sort((a, b) => b - a)
+}
+
+// алгоритм быстрой сортировки
+function sortDesc2(array) {
+    if (array.length < 2) {
+        return array
+    } else {
+        let pivot = array[0]
+        let less = array.slice(1).filter(elem => elem < pivot)
+        let greater = array.slice(1).filter(elem => elem > pivot)
+
+        return sortDesc2(greater).concat([pivot].concat(sortDesc2(less)))
+    }
+}
+
+const numbers = [-20, -10, 0, 10, 20, 30]
+console.log('Первая реализация:')
+console.log(sortDesc1(numbers))
+console.log('Вторая реализация:')
+console.log(sortDesc2(numbers))
