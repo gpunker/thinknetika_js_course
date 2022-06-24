d2e99ab2ba1b99e1a74f3d62bab003222dcf5cba
Alexandr Golovachev <gpunker@gmail.com>
2022-06-24 12:28:04 +0700

    add custom realization

--- lesson2/array_filter.js
+++ lesson2/array_filter.js
@@ -5,12 +5,32 @@
  * filterByLength(fruits, 0,5) // [‘apple’, ‘’]
  */
 
-function filterByLength(array, ...lengths) {
+// реализация с использованием функции фильтрации
+function filterByLength1(array, ...lengths) {
     return array.filter(elem => lengths.includes(elem.length))
 }
 
+// самостоятельная реализация
+function filterByLength2(array, ...lengths) {
+    let filtered = []
+
+    array.forEach(elem => {
+         if (lengths.includes(elem.length)) {
+            filtered.push(elem)
+        }
+    })
+
+    return filtered
+}
+
 const fruits = ['orange', 'apple', 'banana', '']
-console.log(filterByLength(fruits, 0, 5))
+console.log('Первая реализация:')
+console.log(filterByLength1(fruits, 0, 5))
+console.log('Вторая реализация:')
+console.log(filterByLength2(fruits, 0, 5))
 
 const cars = ['Honda', 'Toyota', 'BMW', 'Lada', 'Opel']
-console.log(filterByLength(cars, 4, 3))
+console.log('Первая реализация:')
+console.log(filterByLength1(cars, 4, 3))
+console.log('Вторая реализация:')
+console.log(filterByLength2(cars, 4, 3))
