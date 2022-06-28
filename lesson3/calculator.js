501c2b82309c1966ad5511c8d7663f53ea8ca430
Alexandr Golovachev <gpunker@gmail.com>
2022-06-29 00:29:42 +0700

    + calculator

---
+++ lesson3/calculator.js
@@ -0,0 +1,69 @@
+// 3. Калькулятор
+// 
+// Создайте калькулятор позволяющий добавлять в него дополнительные методы и сохранять результат, 
+// по умолчанию должны присутствовать методы add, substract
+// 
+// Пример: 
+// const calculator = new Calc()
+// 
+// calculator.operation('31 + 32') // 63
+// calculator.operation('10 * 2') // 10
+// calculator.addOperation('/', (a, b) => a / b)
+// calculator.operation('10 / 2') // 5
+// 
+// Также, он должен хранить историю всех операций и выводить ее по запросу:
+//  
+// calculator.history() /* [{operation: '+', operands: [31,32]}, {operation: '*', 
+// operands: [10,2]}, {operation: '/', operands: [10,2]}] */
+//  
+// И очищать историю
+// calculator.clearHistory()
+// calculator.history() // []
+
+function Calc() {
+    let history = []
+    let operations = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b
+    }
+
+    function addHistory(operation, ...operands) {
+        history.push({ operation: operation, operands: operands })
+    }
+  
+    return {
+      operation: (expression) => {
+        let tokenized = expression.split("").filter((i) => i != ' ')
+        let a = parseInt(tokenized[0])
+        let b = parseInt(tokenized[2])
+        let operation = tokenized[1]
+
+        addHistory(operation, a, b)
+
+        try {
+            return operations[operation](a, b)
+        } catch (error) {
+            return 'Error! Некоректное выражение'
+        }
+      },
+      addOperation: (operation, func) => {
+        operations[operation] = func
+      },
+      history: () => history,
+      clearHistory: () => {
+        history = []
+        return
+      }
+    }
+  }
+  
+  const calculator = new Calc()
+  console.log(calculator)
+  console.log(calculator.operation('1 + 2'))
+  console.log(calculator.history())
+  calculator.addOperation('*', (a, b) => a * b)
+  console.log(calculator.operation('2*2'))
+  console.log(calculator.operation('*22'))
+  console.log(calculator.history())
+  calculator.clearHistory()
+  console.log(calculator.history())
