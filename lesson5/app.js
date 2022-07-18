7f41d1125d64f68da15aa4cf4654db2f688a49e2
Alexandr Golovachev <gpunker@gmail.com>
2022-07-18 13:19:01 +0700

    generate figures on desk

--- lesson5/app.js
+++ lesson5/app.js
@@ -1,110 +1,127 @@
 // фигуры
 class BaseFigure {
-    constructor(color) {
+    constructor(color, desk) {
         this.color = color
+        this.desk = desk
     }
 }
 
 class Pawn extends BaseFigure {
-    constructor(color) {
-        super(color)
+    constructor(color, desk) {
+        super(color, desk)
         this.canEvolve = false
     }
 }
 
 class Horse extends BaseFigure {
-    constructor(color) {
-        super(color)
+    constructor(color, desk) {
+        super(color, desk)
     }
 }
 
 class Bishop extends BaseFigure {
-    constructor(color) {
-        super(color)
+    constructor(color, desk) {
+        super(color, desk)
     }
 }
 
 class Rook extends BaseFigure {
-    constructor(color) {
-        super(color)
+    constructor(color, desk) {
+        super(color, desk)
     }
 }
 
 class Queen extends BaseFigure {
-    constructor(color) {
-        super(color)
+    constructor(color, desk) {
+        super(color, desk)
     }
 }
 
 class King extends BaseFigure {
-    constructor(color) {
-        super(color)
+    constructor(color, desk) {
+        super(color, desk)
     }
 }
 
+// доска
 class Desk {
-    constructor() {}
+    constructor() {
+        // генерируем координаты
+        let coords = {}
 
-    #figures = {
+        Desk.x.forEach(function(x) {
+            return Desk.y.forEach(function(y) {
+                return coords[`${x}${y}`] = {}
+            })
+        })
+
+        // генерируем фигуры на доске
         // белые
-        'A1': new Rook('white'),
-        'B1': new Horse('white'),
-        'C1': new Bishop('white'),
-        'D1': new Queen('white'),
-        'E1': new King('white'),
-        'F1': new Bishop('white'),
-        'G1': new Horse('white'),
-        'H1': new Rook('white'),
-        'A2': new Pawn('white'),
-        'B2': new Pawn('white'),
-        'C2': new Pawn('white'),
-        'D2': new Pawn('white'),
-        'E2': new Pawn('white'),
-        'F2': new Pawn('white'),
-        'G2': new Pawn('white'),
-        'H2': new Pawn('white'),
+        coords['A1'] = new Rook('white', this),
+        coords['B1'] = new Horse('white', this),
+        coords['C1'] = new Bishop('white', this),
+        coords['D1'] = new Queen('white', this),
+        coords['E1'] = new King('white', this),
+        coords['F1'] = new Bishop('white', this),
+        coords['G1'] = new Horse('white', this),
+        coords['H1'] = new Rook('white', this),
+        coords['A2'] = new Pawn('white', this),
+        coords['B2'] = new Pawn('white', this),
+        coords['C2'] = new Pawn('white', this),
+        coords['D2'] = new Pawn('white', this),
+        coords['E2'] = new Pawn('white', this),
+        coords['F2'] = new Pawn('white', this),
+        coords['G2'] = new Pawn('white', this),
+        coords['H2'] = new Pawn('white', this),
         // черные
-        'A7': new Pawn('black'),
-        'B7': new Pawn('black'),
-        'C7': new Pawn('black'),
-        'D7': new Pawn('black'),
-        'E7': new Pawn('black'),
-        'F7': new Pawn('black'),
-        'G7': new Pawn('black'),
-        'H7': new Pawn('black'),
-        'A8': new Rook('black'),
-        'B8': new Horse('black'),
-        'C8': new Bishop('black'),
-        'D8': new Queen('black'),
-        'E8': new King('black'),
-        'F8': new Bishop('black'),
-        'G8': new Horse('black'),
-        'H8': new Rook('black'),
+        coords['A7'] = new Pawn('black', this),
+        coords['B7'] = new Pawn('black', this),
+        coords['C7'] = new Pawn('black', this),
+        coords['D7'] = new Pawn('black', this),
+        coords['E7'] = new Pawn('black', this),
+        coords['F7'] = new Pawn('black', this),
+        coords['G7'] = new Pawn('black', this),
+        coords['H7'] = new Pawn('black', this),
+        coords['A8'] = new Rook('black', this),
+        coords['B8'] = new Horse('black', this),
+        coords['C8'] = new Bishop('black', this),
+        coords['D8'] = new Queen('black', this),
+        coords['E8'] = new King('black', this),
+        coords['F8'] = new Bishop('black', this),
+        coords['G8'] = new Horse('black', this),
+        coords['H8'] = new Rook('black', this)
+
+        this.#coords = coords
     }
+
+    #coords = {}
 
     static get y() { return [1, 2, 3, 4, 5, 6, 7, 8] }
     static get x() { return ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'] }
 
-    get figures() { return this.#figures }
+    get coords() { return this.#coords }
 }
 
+// игра
 class Game {
     constructor() {
     }
     
+    #state = Game.states[0]
     #desk = new Desk()
     #turn = 'white'
     #history = []
     #killedFigures = []
     #turnCount = 0
 
+    static get states() { return ['continue', 'white wins', 'black wins'] }
+
     get desk() { return this.#desk }
     get turn() { return this.#turn }
     get history() { return this.#history }
     get killedFigures() { return this.#killedFigures }
     get turnCount() { return this.#turnCount }
+    get state() { return this.#state }
 }
 
-const pawn = new Pawn('white')
-console.log(pawn)
-console.log((new Game).desk)
+console.log((new Game).desk.coords)
