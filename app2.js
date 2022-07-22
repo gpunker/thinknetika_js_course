6f8309d745c8d942f1e0507ff7345bbca1174d3b
Alexandr Golovachev <gpunker@gmail.com>
2022-07-22 12:38:21 +0700

    task2

--- app2.js
+++ app2.js
@@ -1,1 +1,23 @@
 // Реализовать функцию для отображения на экране вложенных массивов.
+function fill(rootElement, content) {
+    if (content instanceof Array) {
+        const list = document.createElement('ul')
+
+        content.forEach(element => {
+            fill(list, element)
+        });
+
+        rootElement.append(list)
+    } else {
+        const li = document.createElement('li')
+        const text = document.createTextNode(content)
+        li.append(text)
+        rootElement.append(li)
+    }
+}
+
+
+const data = [['item6', 'item7'], 'item1', 'item2', ['item3', ['item4', 'item5']]]
+
+const root = document.querySelector('#content')
+fill(root, data)
