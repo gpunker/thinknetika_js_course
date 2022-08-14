2a25ba0e58567aec54762b8eb3d89be4cee7d936
Alexandr Golovachev <gpunker@gmail.com>
2022-08-14 17:32:04 +0700

    custom promise

--- lesson8/custom_promise.js
+++ lesson8/custom_promise.js
@@ -1,40 +1,130 @@
+const STATES = {
+    PENDING: 'pending',
+    FULFILLED: 'fulfilled',
+    REJECTED: 'rejected',
+}
+
 class CustomPromise {
+    _state = STATES.PENDING
+    _result = undefined
+    _fnResolve = this._resolve.bind(this)
+    _fnReject = this._reject.bind(this)
+    _thenCallbacks = []
+    _catchCallbacks = []
+
     constructor(executor) {
-        setTimeout(() => executor(this.resolve, this.reject), 0)
+        try {
+            executor(this._fnResolve, this._fnReject)
+        } catch(e) {
+            this.catch(e)
+        }
+        
     }
 
-    _state = 'pending'
-    // _result = undefined
-    // _error = undefined
+    _resolve(value) {
+        setTimeout(() => this._exec(value, 'resolve'), 0)
+    }
 
-    resolve(result) {
-        if (this.#isPending()) {
-            this._state = 'fulfilled'
-            this._result = result
+    _reject(value) {
+        setTimeout(() => this._exec(value, 'reject'), 0)
+    }
 
-            return this
+    _exec(value, cbName) {
+        if (!this._isPending()) return
+
+        if (value instanceof CustomPromise) {
+            value.then(this._fnResolve, this._fnReject)
+            return
+        }
+        
+        if (cbName === 'resolve') {
+            this._state = STATES.FULFILLED
+        } else {
+            this._state = STATES.REJECTED
+        }
+
+        this._result = value
+        this._runCallbacks()
+    }
+
+    _isPending() { return this._state === STATES.PENDING }
+
+    _runCallbacks() {
+        if (this._state === STATES.FULFILLED) {
+            this._thenCallbacks.forEach((cb) => {
+                cb(this._result)
+            })
+            
+            this._thenCallbacks = []
+        }
+
+        if (this._state === STATES.REJECTED) {
+            this._catchCallbacks.forEach((cb) => {
+                cb(this._result)
+            })
+
+            this._catchCallbacks = []
         }
     }
 
-    reject(error) {
-        if (this.#isPending()) {
-            this._state = 'rejected'
+    then(onfulfilled, onrejected) {
+        return new CustomPromise((resolve, reject) => {
+            this._thenCallbacks.push(result => {
+                if (onfulfilled == null) {
+                    resolve(result)
+                    return
+                }
 
-            return this
-        }
+                try {
+                    resolve(onfulfilled(result))
+                } catch (error) {
+                    reject(error)
+                }
+            })
+
+            this._catchCallbacks.push(result => {
+                if (onrejected == null) {
+                    reject(result)
+                    return
+                }
+
+                try {
+                    resolve(onrejected(result))
+                } catch (error) {
+                    reject(error)
+                }
+            })
+    
+            this._runCallbacks()
+        })
     }
 
-    #isPending = () => this._state === 'pending'
-
-    then(result) {
-
+    catch(reason) {
+        then(null, reason)
     }
 
-    catch(error) {
-
+    finally(onfinally) {
+        return this.then(result => {
+            onfinally()
+            return result
+        }, result => {
+            onfinally()
+            throw result
+        })
     }
 
-    finally() {
+    static resolve(value) {
+        return new CustomPromise(resolve => resolve(value))
+    }
 
+    static reject(value) {
+        return new CustomPromise((resolve, reject) => reject(value))
     }
 }
+
+let p = new CustomPromise(
+    (resolve, reject) => {
+        resolve(1)
+    }
+)
+p.then((value) => value + 1).then((value) => console.log(value))
