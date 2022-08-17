965ba0c0eb71847d92f8e953270639fa8d922d92
Alexandr Golovachev <gpunker@gmail.com>
2022-08-18 00:12:26 +0700

    draft of calculator tests

---
+++ calculator.test.js
@@ -0,0 +1,37 @@
+describe('Калькулятор', () => {
+    describe('Calc::', () => {
+        describe('#operation', () => {
+            describe('exec default operation', () => {
+                describe('+',  () => {
+                    it('adds a new operation to calculator',  () => {
+                    
+                    })
+                })
+    
+                describe('-',  () => {
+                    it('adds a new operation to calculator',  () => {
+                    
+                    })
+                })
+            })
+        })
+
+        describe('#addOperation', () => {
+            it('adds a new operation to calculator',  () => {
+                
+            })
+        })
+
+        describe('#history', () => {
+            it('return an array of operation objects',  () => {
+                
+            })
+        })
+
+        describe('#clearHistory', () => {
+            it('clears calculator history',  () => {
+                
+            })
+        })
+    })
+})
