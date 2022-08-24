c76f9339867ab95acb7a77d4c26adc431646d8e6
Alexandr Golovachev <gpunker@gmail.com>
2022-08-24 22:37:31 +0700

    fix moves for horse

--- src/js/chess_engine/figures/horse.js
+++ src/js/chess_engine/figures/horse.js
@@ -1,3 +1,5 @@
+import { COLORS } from "../constants/colors"
+import Desk from "../desk"
 import Figure from "./figure"
 
 export default class Horse extends Figure {
@@ -6,6 +8,37 @@ export default class Horse extends Figure {
     }
 
     moves() {
-        return 'Ходы для коня'
+        const coords = []
+
+        const ms = [
+            [2, -1],
+            [2, 1],
+            [1, -2],
+            [1, 2],
+            [-2, -1],
+            [-2, 1],
+            [-1, -2],
+            [-1, 2],
+        ]
+
+        const cellX = Desk.x.indexOf(this.cell[0])
+        const cellY = Desk.y.indexOf(this.cell[1])
+        let newX = null
+        let newY = null
+
+        for (let i = 0; i < ms.length; i++) {
+            newX = Desk.x[cellX + ms[i][0]]
+            newY = Desk.y[cellY + ms[i][1]]
+
+            const coordsIndex = `${newX}${newY}`
+
+            if (this.desk.coords[coordsIndex] === null || 
+                (this.desk.coords[coordsIndex] && this.desk.coords[coordsIndex].color !== this.color)
+            ) {
+                coords.push(coordsIndex)
+            }
+        }
+
+        return coords
     }
 }
