d27335593c9052842c34f596bccb7de08f109294
Alexandr Golovachev <gpunker@gmail.com>
2022-08-24 21:53:16 +0700

    pawn moveset

--- src/js/chess_engine/figures/pawn.js
+++ src/js/chess_engine/figures/pawn.js
@@ -1,4 +1,5 @@
-import Figure from "./base"
+import Desk from "../desk"
+import Figure from "./figure"
 
 export default class Pawn extends Figure {
     constructor(color, desk, cell) {
@@ -11,6 +12,30 @@ export default class Pawn extends Figure {
     get canEvolve() { return this._canEvolve } 
 
     moves() {
-        return 'Ходы для пешки'
+        const coords = []
+
+        const ms = [[0,1], [-1, 1], [1,1]]
+        if (this._doubleMove) ms.push([0,2])
+
+        let cellX = Desk.x.indexOf(this.cell[0])
+        let cellY = Desk.y.indexOf(this.cell[1])
+
+        for (let i = 0; i < ms.length; i++) {
+            let newX = Desk.x[cellX + ms[i][0]]
+            let newY = Desk.y[cellY + ms[i][1]]
+
+            let coordsIndex = `${newX}${newY}`
+
+            if (this.desk.coords[coordsIndex] !== undefined) {
+                console.log(ms[i][0], this.desk.coords[coordsIndex] === null)
+                // в теории сюда можно запихать блок для доп обработки
+                if (ms[i][0] !== 0 && this.desk.coords[coordsIndex] === null) {
+                    continue
+                }
+                coords.push(coordsIndex)
+            }
+        }
+
+        return coords
     }
 }
