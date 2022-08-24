2cdec82f276e7640728d84f4d75c10d84db6844c
Alexandr Golovachev <gpunker@gmail.com>
2022-08-24 22:26:40 +0700

    fix init desk

--- src/js/chess_engine/desk.js
+++ src/js/chess_engine/desk.js
@@ -23,13 +23,14 @@ export default class Desk {
         coords['G1'] = new Horse(COLORS.WHITE, this, 'G1')
         coords['H1'] = new Rook(COLORS.WHITE, this, 'H1')
 
+        // белые
         for (let i = 0; i < Desk.x.length; i++) {
             coords[`${Desk.x[i]}2`] = new Pawn(COLORS.WHITE, this, `${Desk.x[i]}2`)
         }
         
         // черные
         for (let i = 0; i < Desk.x.length; i++) {
-            coords[`${Desk.x[i]}7`] = new Pawn(COLORS.WHITE, this, `${Desk.x[i]}7`)
+            coords[`${Desk.x[i]}7`] = new Pawn(COLORS.BLACK, this, `${Desk.x[i]}7`)
         }
         coords['A8'] = new Rook(COLORS.BLACK, this, 'A8'),
         coords['B8'] = new Horse(COLORS.BLACK, this, 'B8'),
@@ -45,7 +46,7 @@ export default class Desk {
 
     _coords = {}
 
-    static get y() { return ['1', '2', '3', '4', '5', '6', '7', 8] }
+    static get y() { return ['1', '2', '3', '4', '5', '6', '7', '8'] }
     static get x() { return ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'] }
 
     get coords() { return this._coords }
