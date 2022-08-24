64ad0317d52c31995735513cefb44451fcf6883d
Alexandr Golovachev <gpunker@gmail.com>
2022-08-24 23:58:29 +0700

    dev env

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'main.[fullhash].js',
