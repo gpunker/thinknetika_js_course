c1906d16ecfbbe1f060faa1524eddc0e9de780d3
Alexandr Golovachev <gpunker@gmail.com>
2022-08-25 11:07:53 +0700

    horse moves

--- src/js/chess_engine/figures/horse.js
+++ src/js/chess_engine/figures/horse.js
@@ -9,35 +9,44 @@ export default class Horse extends Figure {
 
     moves() {
         const coords = []
-
-        const ms = [
-            [2, -1],
-            [2, 1],
-            [1, -2],
-            [1, 2],
-            [-2, -1],
-            [-2, 1],
-            [-1, -2],
-            [-1, 2],
-        ]
-
         const cellX = Desk.x.indexOf(this.cell[0])
         const cellY = Desk.y.indexOf(this.cell[1])
-        let newX = null
-        let newY = null
 
-        for (let i = 0; i < ms.length; i++) {
-            newX = Desk.x[cellX + ms[i][0]]
-            newY = Desk.y[cellY + ms[i][1]]
+        // ходы вверх
+        for (let i = -2; i <= 2; i++) {
+            for (let j = -2; j <= 2; j++) {
+                if (
+                    (([-1, 1].includes(i)) && ([-2, 2].includes(j))) ||
+                    (([-2, 2].includes(i)) && ([-1,1].includes(j)))
+                ) {
+                    let c = `${Desk.x[cellX + i]}${Desk.y[cellY + j]}`
+                    
+                    if (
+                        this.desk.coords[c] == null ||
+                        (this.desk.coords[c] && this.desk.coords[c].color != this.color)
+                    ) {
+                        coords.push(c)
+                    }
 
-            const coordsIndex = `${newX}${newY}`
-
-            if (this.desk.coords[coordsIndex] === null || 
-                (this.desk.coords[coordsIndex] && this.desk.coords[coordsIndex].color !== this.color)
-            ) {
-                coords.push(coordsIndex)
+                }
             }
         }
+
+
+
+
+        // for (let i = 0; i < ms.length; i++) {
+        //     newX = Desk.x[cellX + ms[i][0]]
+        //     newY = Desk.y[cellY + ms[i][1]]
+
+        //     const coordsIndex = `${newX}${newY}`
+
+        //     if (this.desk.coords[coordsIndex] === null || 
+        //         (this.desk.coords[coordsIndex] && this.desk.coords[coordsIndex].color !== this.color)
+        //     ) {
+        //         coords.push(coordsIndex)
+        //     }
+        // }
 
         return coords
     }
