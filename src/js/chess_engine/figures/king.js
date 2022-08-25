46b0f7c6077843fa6b0a30e1e73152882f3fba32
Alexandr Golovachev <gpunker@gmail.com>
2022-08-25 13:44:17 +0700

    king moves

--- src/js/chess_engine/figures/king.js
+++ src/js/chess_engine/figures/king.js
@@ -1,3 +1,4 @@
+import Desk from "../desk"
 import Figure from "./figure"
 
 export default class King extends Figure {
@@ -6,6 +7,25 @@ export default class King extends Figure {
     }
 
     moves() {
-        return 'Ходы для короля'
+        const coords = []
+        const cellX = Desk.x.indexOf(this.cell[0])
+        const cellY = Desk.y.indexOf(this.cell[1])
+
+        for (let x = -1; x <= 1; x++) {
+            for (let y = -1; y <= 1; y++) {
+                if (x === 0 && y === 0) continue
+
+                let c = `${Desk.x[cellX + x]}${Desk.y[cellY + y]}`
+
+                if (
+                    this.desk.coords[c] === null || 
+                    this.desk.coords[c] && this.desk.coords[c].color !== this.color
+                ) {
+                    coords.push(c)
+                }
+            }
+        }
+
+        return coords
     }
 }
